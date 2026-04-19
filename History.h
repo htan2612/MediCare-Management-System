d07d94d86e677d783f2228cd38027aff887762a6
DuyPhuoc509 <24127509@student.hcmus.edu.vn>
2026-04-20 01:11:42 +0700

    Cập nhật lại struct VisitNode và bổ sung hàm deleteVisitHistory

--- History.h
+++ History.h
@@ -1,22 +1,23 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
-#include <iostream>
 #include <string>
 #include "Patient.h"
-using namespace std;
 
+//  thêm lại trường symptoms (Triệu chứng)cho đúng yêu cầu ban đầu
 struct VisitNode {
-    string date;
-    string diagnosis;
-    string prescription;
+    std::string date;
+    std::string symptoms;     
+    std::string diagnosis;
+    std::string prescription;
     VisitNode* next;
 };
 
-VisitNode* createVisitNode(const string& date, const string& diagnosis, const string& prescription);
+// Khai báo các hàm
+VisitNode* createVisitNode(const std::string& date, const std::string& symptoms, const std::string& diagnosis, const std::string& prescription);
 void initHistory(Patient& p);
-void addVisitHistory(Patient& p, const string& date, const string& diagnosis, const string& prescription);
+void addVisitHistory(Patient& p, const std::string& date, const std::string& symptoms, const std::string& diagnosis, const std::string& prescription);
 void displayVisitHistory(const Patient& p);
-void deleteVisitHistory(Patient& p);
+void deleteVisitHistory(Patient& p); // Hàm mới: Xóa lịch sử để giải phóng bộ nhớ
 
 #endif
